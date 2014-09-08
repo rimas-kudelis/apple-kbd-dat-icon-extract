2e88e156f7ea782a870bda187c6feb8e7c9eaa8d
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-09-08 16:05:45 -0700

    added LanguageRecognizer class; language labels written too

--- apple-kbd-dat-icon-extract.py
+++ apple-kbd-dat-icon-extract.py
@@ -14,6 +14,11 @@ import os
 class DataRecognizer(object):
     header = 'start'
     footer = 'end'
+    name = None
+
+
+    recognizers = []
+
 
     @classmethod
     def isDataStart(cls, buf, pos):
@@ -36,10 +41,10 @@ class DataRecognizer(object):
         raise NotImplemented
 
 
-
 class ICNSRecognizer(DataRecognizer):
     header = 'icns'
     footer = 'IEND\xae\x42\x60\x82'
+    name = 'ICNS'
 
     @classmethod
     def isDataStart(cls, buf, pos):
@@ -48,11 +53,10 @@ class ICNSRecognizer(DataRecognizer):
 
     @classmethod
     def findNextDataStart(cls, buf, pos):
-        while pos < len(buf):
-            if cls.isDataStart(buf, pos):
-                return pos
-            pos += 1
-        return None
+        if pos >= len(buf):
+            return None
+        pos = buf.find(cls.header, pos)
+        return pos if pos != -1 else None
 
 
     @classmethod
@@ -80,6 +84,112 @@ class ICNSRecognizer(DataRecognizer):
         return cls.findDataRange(buf, origin)
 
 
+class LanguageRecognizer(DataRecognizer):
+    header = None
+    footer = None
+    name = 'LANG'
+
+    @classmethod
+    def isDataStart(cls, buf, pos):
+        return buf[pos].isalpha()
+
+
+    @classmethod
+    def findNextDataStart(cls, buf, pos):
+        if pos >= len(buf):
+            return None
+        while buf[pos] == '\x00':
+            pos += 1
+            if pos >= len(buf):
+                return None
+        if not buf[pos].isalpha():
+            return None
+        return pos
+
+
+    @classmethod
+    def readCString(cls, buf, pos):
+        origin = pos
+        pos = buf.find('\x00', origin)
+        if pos == -1:
+            return None
+        return buf[origin:pos]
+
+    @classmethod
+    def readData(cls, buf, pos):
+        origin = pos
+        assert cls.isDataStart(buf, origin)
+        name = cls.readCString(buf, origin)
+        if name is None:
+            return None
+        pos += len(name) + 1
+        if pos >= len(buf):
+            return None
+        pos = cls.findNextDataStart(buf, pos)
+        if pos is None:
+            return None
+        code = cls.readCString(buf, pos)
+        if code is not None:
+            pos += len(code) + 1
+        if pos >= len(buf):
+            return None
+        result = {
+            'origin': origin,
+            'size': pos - origin,
+            'name': name,
+            'code': code,
+        }
+        print(result)
+        return result
+
+
+    @classmethod
+    def findDataSize(cls, buf, pos):
+        assert cls.isDataStart(buf, pos)
+        origin = pos
+        pos = buf.find('\x00', pos)
+        if pos == -1:
+            return None
+        pos = cls.findNextDataStart(buf, pos)
+        if pos is None:
+            return None
+        pos = buf.find('\x00', pos)
+        if pos == -1:
+            return None
+        pos += 1
+        if pos >= len(buf):
+            return None
+        return pos - origin
+
+
+    @classmethod
+    def findDataRange(cls, buf, pos):
+        origin = pos
+        size = cls.findDataSize(buf, pos)
+        if size is None:
+            return None
+        return (origin, size)
+
+
+    @classmethod
+    def findNextDataRange(cls, buf, pos):
+        pos = cls.findNextDataStart(buf, pos)
+        if pos is None:
+            return None
+        r = cls.findDataRange(buf, pos)
+        if r is None:
+            return None
+        return r
+
+
+    @classmethod
+    def readNextData(cls, buf, pos):
+        pos = cls.findNextDataStart(buf, pos)
+        if pos is None:
+            return None
+        return cls.readData(buf, pos)
+
+
 def writeBuffer(filename, buf):
     with open(filename, 'wb') as f:
         f.write(buf)
@@ -93,7 +203,7 @@ def extractData(buf):
     origin = 0
     pos = 0
     results = []
-    while pos < len(buf):
+    while True:
         r = ICNSRecognizer.findNextDataRange(buf, origin)
         if r is None:
             break
@@ -103,6 +213,28 @@ def extractData(buf):
         origin = pos + size
     if origin < len(buf):
         results.append(('UNKNOWN', buf[origin:len(buf)]))
+
+    # try to get labels for the parts
+    temp = results
+    results = []
+    for i, (type, data) in enumerate(temp):
+        if type == 'ICNS':
+            results.append((type, data))
+            continue
+
+        # elif type == 'UNKNOWN':
+        r = LanguageRecognizer.findNextDataRange(data, 0)
+        print str(i) + ':' + repr(r)
+        if r is None:
+            results.append(('UNKNOWN', data))
+            continue
+        if r[0] != 0 and not all(b == '\x00' for b in data[0:r[0]]):
+            results.append(('UNKNOWN', data[0:r[0]]))
+        end = sum(r)
+        results.append(('LANG', data[r[0]:end]))
+        if end < len(data):
+            results.append(('UNKNOWN', data[end:]))
+
     return results
 
 
@@ -113,13 +245,25 @@ def writeAllData(items, outputDir):
 
     exts = {
         'UNKNOWN': 'dat',
+        'LANG': 'txt',
         'ICNS': 'icns',
     }
 
+    name = None
+    code = None
     for index, (type, data) in enumerate(items):
+        if type == 'LANG':
+            labelData = LanguageRecognizer.readNextData(data, 0)
+            if labelData:
+                name = labelData['name']
+                code = labelData['code']
+            else:
+                name = None
+                code = None
+
         filename = outputDir + '/' + filenameFormat.format(
             index=index,
-            type=type,
+            type=type if type == 'UNKNOWN' or not name else name + ' (' + code + ')' or type,
             ext=exts[type],
         )
 
